c974e544bb4977419b7f205967c3c902575d9b14
Pierre Litoux <pierre.litoux@epita.fr>
2021-03-27 16:56:24 +0100

    Pour adrien

--- Projet/Noeud_de_minage/minage.c
+++ Projet/Noeud_de_minage/minage.c
@@ -1,6 +1,10 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <err.h>
+#include <sys/wait.h>
+#include <signal.h>
 #include "../Hash/sha256.h"
 
 //read stdin "prevhash\ntimestamp\nindex\ntransaction"
@@ -8,12 +12,10 @@
 //write stdout "prevhash\ntimestamp\nindex\ntransaction\nproof"
 //write : string(either : "occupied" or "proof")
 //
-//(infos of the current block)
-//char *info[prevhash,timestamp,index,transaction,proof]
 
 //string to sum : takes the incoming string and sum it's component splitted with \n
 unsigned int char_to_sum(char str[]){
-	char delim[] = "\n";
+	char delim[] = "-";
 	char *token = strtok(str, delim);
 	unsigned int res = 0;
 	while(token != NULL)
@@ -40,9 +42,9 @@ unsigned long mine(int diff, unsigned int sum){
 		sha256(str, buf);
 		//uncomment if you want to print the hash
 		/*printf("buf :");
-		for(int i = 0; i < 32; i++)
-			printf("%02x", buf[i]);
-		printf("\n");*/
+		  for(int i = 0; i < 32; i++)
+		  printf("%02x", buf[i]);
+		  printf("\n");*/
 		res = 0;
 		for(int i = SHA256_BLOCK_SIZE - diff; i < SHA256_BLOCK_SIZE; i++)
 		{
@@ -56,7 +58,7 @@ unsigned long mine(int diff, unsigned int sum){
 }
 
 //return 0 if proof is correct, 1 if not
-unsigned int testproof(int diff, unsigned int sum, unsigned long proof){
+unsigned int __testproof(int diff, unsigned int sum, unsigned long proof){
 	unsigned int res = 0;
 	unsigned int tmp;
 	BYTE str[SHA256_BLOCK_SIZE];
@@ -72,6 +74,15 @@ unsigned int testproof(int diff, unsigned int sum, unsigned long proof){
 		if(buf[i] != 0)
 			res = 1;
 	}
+	return res;
+}
+
+//test the proof witout splitting strin return 0 if proof is correct, 1 if not
+unsigned int testproof(int diff, char strin[] ,unsigned long proof){
+	char *str = malloc(sizeof(char) * strlen(strin));
+	strcpy(str,strin);
+	unsigned int res = __testproof(diff, char_to_sum(str), proof);
+	free(str);
 	return res;
 }
 
@@ -106,7 +117,7 @@ void rebuild(char* ptr, char *str, unsigned long proof){
 		ptr[i] = str[i];
 		i++;
 	}
-	ptr[i] = '\n';
+	ptr[i] = '-';
 	i++;
 	size_t j = 0;
 	while(strproof[j] != '\0'){
@@ -116,51 +127,124 @@ void rebuild(char* ptr, char *str, unsigned long proof){
 	ptr[i+j] = '\0';
 }
 
-//read on fdin and send the proff on fdout
-//void Node(int fdin, int fdout){
 
-//}
-
-int moncuq()
+/*
+//write on fd
+void rewrite(int fd, const void *buf, size_t count)
 {
+	ssize_t res = 0;
+	ssize_t tmp = 0;
+	while((res = write(fd, buf + tmp, count)) < (ssize_t)count - tmp){
+		if(res == -1)
+			err(3,"error while writting");
+		tmp += res;
+	}
+}
+
+//take a string and send it with the proof on fdout
+void mine_and_write(int diff, char str[], int fdout){
+	unsigned long proof = mine_from_string(diff,str);
+	char strout[len_of_proof(proof) + strlen(str) + 2];
+	rebuild(strout, str, proof);
+	rewrite(fdout, &strout, strlen(strout));
+}
+*/
+/* beta test
+int occupied = 0;
+
+void handler(){
+	wait(NULL);
+	occupied = 0;
+}
+
+
+//basic server wich read on fdin fork a node and send the res on fdout, if occupied send "occupied"
+int mininglobby(int fdin, int fdout){
+	ssize_t r = 0;
+	int diff = 3;
+	//int occupied = 0;
+	size_t lenbuff = 512;
+	char buff[] = "111-222-333-555";
+	pid_t p = getpid();
+	while(p != 0){
+		while(r == 0){
+			//write(fdout, "111-222-333-555", 15);
+			r = read(fdin, buff, lenbuff);
+			//r=1;
+			//sleep(1);
+		}
+		if(r > 0){
+			if(occupied == 1){
+				write(fdout, "occupied", 8);
+			}
+			else{
+				signal(SIGCHLD,handler);
+				if((p = fork()) == 0){//son
+					mine_and_write(diff, buff, fdout);
+				}
+				else{//father
+					occupied = 1;
+				}
+			}
+			r = 0;
+		}
+	}
+	return 0;
+}
+*/
+
+
+//int moncuq()
+//{
+	//mininglobby(STDIN_FILENO, STDOUT_FILENO);
 	//char str[] = "prevhash\ntimestamp\nindex\ntransaction";
-	  char nbstr[] = "1\n2\n3\n5";
-	  /*char *str = malloc(sizeof(char) * strlen(nbstr));
+	//char nbstr[] = "111-222-333-555";
+	/*char *str = malloc(sizeof(char) * strlen(nbstr));
 	  strcpy(str,nbstr);
 	  unsigned int sum = char_to_sum(str);
 	  free(str);
 	  printf("sum : %d\n",sum);
 
 	  unsigned int proof = mine(difficulty,sum);*/
-	  int diff = 2;
-	  unsigned long proof = mine_from_string(diff,nbstr);
+	//int diff = 1;
+	//mine_and_write(diff, nbstr, STDOUT_FILENO);
+	//printf("\n");
+	//unsigned long proof = mine_from_string(diff,nbstr);
 
-	  /*char *str = malloc(sizeof(char) * strlen(nbstr));
-	  strcpy(str,nbstr);
-	  unsigned int res = testproof(diff, char_to_sum(str), proof);
-	  free(str);
-	  if(res == 0)
-		  printf("true\n");
-	  else
-		  printf("false\n");*/
-	  printf("proof : %ld\n", proof);
-	  char strout[len_of_proof(proof) + strlen(nbstr) + 2];
-	  rebuild(strout, nbstr, proof);
-	  printf("str finale : \n%s\n", strout);
-	  printf("str finale v2 : \n");
-	  for(size_t i = 0; i < len_of_proof(proof) + strlen(nbstr) + 2; i++)
-		  printf("%d\n",strout[i]);
-	  printf("\n");
-
+	/*char *str = malloc(sizeof(char) * strlen(nbstr));
+	strcpy(str,nbstr);*/
+	/*unsigned int res = testproof(diff, nbstr, proof);
+	//free(str);
+	if(res == 0)
+		printf("proof is true\n");
+	else
+		printf("proof is false\n");
+	printf("proof : %ld\n", proof);*/
+	//char strout[len_of_proof(proof) + strlen(nbstr) + 2];
+	//rebuild(strout, nbstr, proof);
+	/*printf("str finale : \n%s\n", strout);
+	printf("str finale v2 : \n");
+	for(size_t i = 0; i < len_of_proof(proof) + strlen(nbstr) + 2; i++){
+		if(strout[i] == '\n')
+			printf("\n%d\n",strout[i]);
+		else{
+			if(strout[i] == '\0')
+				printf("\n0");
+			else
+				printf("%d",strout[i]);
+		}
+	}
+	printf("\n");
+*/
 	/*
-	BYTE hash1[SHA256_BLOCK_SIZE] = {0x24,0x8d,0x6a,0x61,0xd2,0x06,0x38,0xb8,0xe5,0xc0,0x26,0x93,0x0c,0x3e,0x60,0x39,0xa3,0x3c,0xe4,0x59,0x64,0xff,0x21,0x67,0xf6,0xec,0xed,0xd4,0x19,0xdb,0x06,0xc1};
-	printf("hash :\n%s\n", hash1);
-	BYTE str[] = {"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"};
-	BYTE buf[SHA256_BLOCK_SIZE];
-	sha256(str, buf);
-	printf("buf :\n");
-	for(int i = 0; i<32; i++)
-		printf("%02x", buf[i]);
-	printf("\n");*/
-	return 0;
-}
+	   BYTE hash1[SHA256_BLOCK_SIZE] = {0x24,0x8d,0x6a,0x61,0xd2,0x06,0x38,0xb8,0xe5,0xc0,0x26,0x93,0x0c,0x3e,0x60,0x39,0xa3,0x3c,0xe4,0x59,0x64,0xff,0x21,0x67,0xf6,0xec,0xed,0xd4,0x19,0xdb,0x06,0xc1};
+	   printf("hash :\n%s\n", hash1);
+	   BYTE str[] = {"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"};
+	   BYTE buf[SHA256_BLOCK_SIZE];
+	   sha256(str, buf);
+	   printf("buf :\n");
+	   for(int i = 0; i<32; i++)
+	   printf("%02x", buf[i]);
+	   printf("\n");*/
+	//return 0;
+//}
