3aae588155d0e3c0fc7eec65cd8e030d1f8e9f9a
thimot <thimot.veyre@gmail.com>
2021-05-29 18:14:11 +0200

    ajout calcul amount of money and general_info.h

--- Projet/Network/informations.h
+++ Projet/Network/informations.h
@@ -1,21 +1,13 @@
 #ifndef INFORMATIONS_H
 #define INFORMATIONS_H
 
+#include "../general_informations.h"
+
 /*
 *   This file contain some informations about the network, there is no .c attach to it
 */
 
-
 #define DEFAULT_PORT "6969"
-
-//Boolean part
-#ifndef TRUE 
-#define TRUE 1
-#endif
-
-#ifndef FALSE 
-#define FALSE 0
-#endif
 
 // Message part
 #define BUFFER_SIZE_SOCKET 512
@@ -24,23 +16,5 @@
 #define SIZE_DATA_LEN_HEADER 8
 #define SIZE_TYPE_MSG sizeof(char)
 #define HEADER_SIZE SIZE_DATA_LEN_HEADER + SIZE_TYPE_MSG
-
-//Type of message
-#define TYPE_NETWORK 1
-#define TYPE_TEST 255
-
-//Status part
-#define ERROR -1
-#define ENDED 0
-#define CONNECTED 1
-#define CONNECTING 3
-#define NOTCONNECTED 4
-#define SENTINEL 5
-#define ONLINE 6
-#define OFFLINE 7
-#define EXITING 8
-#define STARTING 9
-#define NOTUSED 255
-#define DEAD 254
 
 #endif
---
+++ Projet/general_informations.h
@@ -0,0 +1,28 @@
+#ifndef TOOLS_H
+#define TOOLS_H
+
+//BOOLEAN PART
+#define TRUE 1
+#define FALSE 0
+
+//Type of message
+#define TYPE_NETWORK 1
+#define TYPE_TEST 255
+
+//Different info that could be useful
+#define ERROR -1
+#define ENDED 0
+#define CONNECTED 1
+#define CONNECTING 3
+#define NOTCONNECTED 4
+#define SENTINEL 5
+#define NOTUSED 255
+#define DEAD 254
+
+//Status part
+#define ONLINE 6
+#define OFFLINE 7
+#define EXITING 8
+#define STARTING 9
+
+#endif
