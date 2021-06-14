593ed1a715f3996b427923204faebdb7bcb32070
thimot <thimot.veyre@gmail.com>
2021-06-14 14:03:50 +0200

    multithread envoie msg

--- Projet/Network/client.c
+++ Projet/Network/client.c
@@ -1,5 +1,13 @@
 #include "client.h"
 
+struct msgInfo
+{
+	MESSAGE *msg;
+	struct clientInfo *client;
+	int res;
+	pthread_t thread;
+};
+
 /*
 *	Secure the sending of message on a socket
 */
@@ -23,22 +31,21 @@ int Send(int fd, const void *buf, size_t count, int flag)
 int connectClient(struct address address)
 {
 	int skt = -1;
-	struct timeval timeout;      
-    timeout.tv_sec = 1;
-    timeout.tv_usec = 0;
+	struct timeval timeout;
+	timeout.tv_sec = 1;
+	timeout.tv_usec = 0;
 
 	if ((skt = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 		return EXIT_FAILURE;
-    
-    if (setsockopt (skt, SOL_SOCKET, SO_SNDTIMEO, (char *)&timeout,sizeof(timeout)) < 0)
-		return EXIT_FAILURE;
 
+	if (setsockopt(skt, SOL_SOCKET, SO_SNDTIMEO, (char *)&timeout, sizeof(timeout)) < 0)
+		return EXIT_FAILURE;
 
 	struct sockaddr_in *IP = GetIPfromHostname(address);
 
 	if (IP == NULL || connect(skt, (struct sockaddr *)IP, sizeof(struct sockaddr_in)) < 0)
 	{
-		if(IP != NULL)
+		if (IP != NULL)
 			free(IP);
 		fprintf(stderr, "Client: %s:%s disconnected\n", address.hostname, address.port);
 		return EXIT_FAILURE;
@@ -72,21 +79,54 @@ int SendMessageForOneClient(struct clientInfo *client, MESSAGE *message)
 	return res;
 }
 
+void *SendMessageThread(void *arg)
+{
+	struct msgInfo *temp = arg;
+	temp->res = SendMessageForOneClient(temp->client, temp->msg);
+	return NULL;
+}
+
 /*
 *	Take a list of client and message and send the message at each client
-*	If there is a problem with the client, , it will remove of the list
+*	If there is a problem with the client, it will remove of the list
 *
-*	Could be imrove with multithreading
+*	Could be improve with multithreading
 */
 void SendMessage(struct clientInfo *clientList, MESSAGE *message)
 {
+	size_t len = listLen(clientList);
+	size_t i = 0;
+	struct msgInfo *tab = malloc(sizeof(struct msgInfo) * len);
+	if (tab == NULL)
+		return ;
+
 	for (clientList = clientList->sentinel->next; clientList->isSentinel == FALSE; clientList = clientList->next)
 	{
-		if (SendMessageForOneClient(clientList, message) == EXIT_FAILURE)
+		tab[i].msg = message;
+		tab[i].client = clientList;
+
+		if (pthread_create(&tab[i].thread, NULL, SendMessageThread, (void *)tab + i) != 0)
+			tab[i].res = ERROR;
+			
+		i++;
+	}
+
+	i = 0;
+
+	for (clientList = clientList->sentinel->next; clientList->isSentinel == FALSE; clientList = clientList->next)
+	{
+		if (tab[i].res != ERROR)
 		{
-			struct clientInfo *temp = clientList;
-			clientList = clientList->next;
-			removeClient(temp);
+			pthread_join(tab[i].thread, NULL);
+
+			if (tab[i++].res == EXIT_FAILURE)
+			{
+				struct clientInfo *temp = clientList;
+				clientList = clientList->next;
+				removeClient(temp);
+			}
 		}
 	}
+
+	free(tab);
 }
