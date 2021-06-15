8b2d3395ddf158cc5e32ed2d009e2eb630477a3b
thimot <thimot.veyre@gmail.com>
2021-06-15 12:37:23 +0200

    plus de probleme 32bits

--- Projet/Network/network_tools.c
+++ Projet/Network/network_tools.c
@@ -72,9 +72,6 @@ struct clientInfo *addClient(struct clientInfo *list, struct address address, in
 
     struct sockaddr_in *IP = GetIPfromHostname(address);
 
-    printf("Ip ptr: %p\n", IP);
-    
-
     if (IP == NULL || connect(skt, (struct sockaddr *)IP, sizeof(struct sockaddr_in)) < 0)
     {
         if (IP != NULL)
